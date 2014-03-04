6479a046354c6440a26f8d5ef308e220be92998a
Eliana Feasley <eliana@khanacademy.org>
2014-03-03 21:03:42 -0500

    Remove attempt to train on time
    This was causing bugs, and it's not well supported elsewhere in the code
    
    Test Plan: managed to train a new model

--- mirt/mirt_util.py
+++ mirt/mirt_util.py
@@ -376,15 +376,9 @@ def L_dL(theta_flat, user_states, num_exercises, options, pool):
     for r in rslts:
         Lu, dLu, exercise_indu = r
         L += Lu
-        print 'r'
-        print r
-        print 'dl'
-        print dL.W_correct[exercise_indu, :]
-        print 'dLu'
-        print dLu.W_correct
         dL.W_correct[exercise_indu, :] += dLu.W_correct
-        dL.W_time[exercise_indu, :] += dLu.W_time
-        dL.sigma_time[exercise_indu] += dLu.sigma_time
+        #dL.W_time[exercise_indu, :] += dLu.W_time
+        #dL.sigma_time[exercise_indu] += dLu.sigma_time
 
     if options.correct_only:
         dL.W_time[:, :] = 0.
